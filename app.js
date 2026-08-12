05393f77296682362dd033af6e2a0553159d7f27
Your Name <you@example.com>
2026-08-12 10:30:35 +0200

    locations basic done

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ import express from 'express';
 import path from 'node:path';
 
 import routerLocation from './routes/locationRoute.js';
-import routerWheather from './routes/weatherRoute.js';
+import routerWheather from './routes/wheatherRoute.js';
 
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/location", routerLocation);
 
--- routes/locationRoute.js
+++ routes/locationRoute.js
@@ -1,11 +1,11 @@
 import express from "express";
-import formattedUserData from "../controllers/locationController.js";
+import showLocationsData from "../controllers/locationController.js";
 
 const routerLocation = express.Router();
 
 routerLocation.get("/", (req, res, next) => {
     console.log("llegó al router");
     next();
-}, formattedUserData);
+}, showLocationsData);
 
 export default routerLocation;
