48876256a381b2df494d7e19d2f2c772251a1faf
Your Name <you@example.com>
2026-08-25 20:50:02 +0200

    html show cities

--- public/main.js
+++ public/main.js
@@ -1,5 +1,55 @@
 const formulario = document.getElementById("formulario");
 
+function showLocationsHTML(dataLocations) {
+    const main = document.querySelector("main");
+    const tabla = document.createElement("table");
+
+    const atributos = document.createElement("thead");
+    const fila1 = document.createElement("tr");
+    const elemento1 = document.createElement("td");
+    const elemento2 = document.createElement("td");
+    const elemento3 = document.createElement("td");
+    const elemento4 = document.createElement("td");
+    const elemento5 = document.createElement("td");
+    const elemento6 = document.createElement("td");
+
+    elemento1.textContent = "Bandera";
+    elemento2.textContent = "Pais";
+    elemento3.textContent = "Nombre";
+    elemento4.textContent = "Longitud";
+    elemento5.textContent = "Latitud";
+    elemento6.textContent = "Zona horaria";
+
+    fila1.append(elemento1, elemento2, elemento3, elemento4, elemento5, elemento6);
+    atributos.appendChild(fila1);
+    tabla.appendChild(atributos);
+
+    for (const location of dataLocations) {
+        console.log(location.country);
+        const filaNueva = document.createElement("tr");
+
+        const bandera = document.createElement("th");
+        const pais = document.createElement("td");
+        const nombre = document.createElement("td");
+        const longitud = document.createElement("td");
+        const latitude = document.createElement("td");
+        const timezone = document.createElement("td");
+
+        pais.innerText = location.country;
+        nombre.textContent = location.name;
+        longitud.textContent = location.longitude;
+        latitude.textContent = location.latitude;
+        timezone.textContent = location.timezone;
+
+        filaNueva.append(bandera, pais, nombre, longitud, latitude, timezone);
+        tabla.appendChild(filaNueva);
+    };
+
+    main.append(tabla);
+};
+
+
+
 formulario.addEventListener("submit", async (e) => {
     e.preventDefault();
 
@@ -18,5 +68,8 @@ formulario.addEventListener("submit", async (e) => {
     const response = await fetch(`/location?${new URLSearchParams(filteredParams)}`);
     const data = await response.json();
 
-    console.log(data);
+    console.log(data.results)
+    showLocationsHTML(data.results);
 });
+
+
